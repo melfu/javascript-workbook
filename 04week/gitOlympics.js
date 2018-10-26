eca869d4be57ef2199fdcd44c32ac4a67331e48b
melfu <melular@gmail.com>
2018-10-25 19:08:14 -0500

    1

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,8 @@
+'use strict'
+const names = ['mel', 'hannah', 'laura', 'wilson', 'stiles'];
+const printListOfOlympians = (array) => {
+    array.forEach(function(element){
+        console.log(element)
+    })
+}
+printListOfOlympians(names);
