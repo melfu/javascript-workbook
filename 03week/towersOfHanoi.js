ab24d68c9cb04cdb86d2524dddc16b8a8ffb7e54
melfu <melular@gmail.com>
2018-10-30 10:54:50 -0500

    added some movePiece code, can move first piece now!

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -13,49 +13,80 @@ let stacks = {
   c: []
 };
 
-function printStacks() {
+const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
+};
+//------------------------------------------------------
+// const towerOne = Object.values(stacks['a']);
+// const towerTwo = Object.values(stacks['b']);
+// const towerThree = Object.values(stacks['c']);
+// const towerOneIndex = Object.keys(stacks['a']);
+// const towerTwoIndex = Object.keys(stacks['b']);
+// const towerThreeIndex = Object.keys(stacks['c']);
+
+  // moves piece based on user input  
+const movePiece = (startStack, endStack) => {
+  // holds the object key for the array to remove disc from
+  const removePieceHere = stacks[startStack];
+  // holds the object key for the array to add disc to
+  const addPieceHere = stacks[endStack];
+  // should remove last array value from array user entered at start stack prompt
+  const poppedPiece = removePieceHere.pop();
+  // should add last array value that was removed from start stack onto the end stack
+  addPieceHere.push(poppedPiece);
+  console.log(removePieceHere);
+  console.log(addPieceHere);
 }
 
-//moves piece based on user input
-function movePiece() {
-  console.log('removedPiece')
+// const addedPiece = stacks[endStack].push(startStackResult);
+// const removedPiece = towerOne.pop(removedPiece)
+// console.log(removedPiece)
+// console.log(towerOne)
+// towerOne.pop()
+// console.log(towerOne)
+// towerThree.push(removedPiece)
+// console.log(towerThree)
+// towerOne.pop()
+// towerThree.push(removedPiece)
+// console.log(towerThree)
 
-  const removedPiece = stacks.startStack.pop();
-  stacks.push(removedPiece);
-}
+//   const startStackPick = stacks['startStack'];
+//   const startStackResult = startStackPick.pop;
+//   const newStack = [];
+//   stacks.forEach=(item, index) => {
+//     newStack.push(startStackResult)
+//   };
+// }
 
 // checks for user input of a, b or c
-function isValidInput(startStack, endStack) {
+const isValidInput = (startStack, endStack) => {
   if (startStack === 'a' || startStack === 'b' || startStack === 'c') (endStack === 'a' || endStack === 'b' || endStack === 'c') 
   {
-    console.log('it is valid')
-    return true;
+    return true
   } 
 }
 // checks that input follows rules of game : largest piece always on bottom of stack  
-function isLegal(startStack, endStack) {
+const isLegal = (startStack, endStack) => {
 
 }
 
 // checks for a game win
-function checkForWin() { if (stacks === { a: [], b: [4, 3, 2, 1], c: [] } || stacks ===    { a: [], b: [], c: [4, 3, 2, 1] }) {
+const checkForWin = () => { if (stacks === { a: [], b: [4, 3, 2, 1], c: [] } || stacks ===    { a: [], b: [], c: [4, 3, 2, 1] }) {
   console.log('checkin on that win')
   return true
 }
 }
 
 // overall game function called by getPrompt
-function towersOfHanoi(startStack, endStack) {
-  isValidInput(startStack, endStack);
-  console.log('valid')
-  movePiece();
-
+const towersOfHanoi = (startStack, endStack) => { if (
+  isValidInput(startStack, endStack)) {
+  movePiece(startStack, endStack);
 }
-
-function getPrompt() {
+}
+//-------------------------------------------------------------------------
+const getPrompt = () => {
   printStacks();
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
