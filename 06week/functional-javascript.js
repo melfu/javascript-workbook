4f4090f39d8c2f5f88c9ed990979a3881da86a68
melfu <melular@gmail.com>
2018-12-11 15:21:59 -0600

    corrected first 2 or 3 renee comments

---
+++ 06week/functional-javascript.js
@@ -0,0 +1,11 @@
+function upperCaser(input) {
+    input.toUpperCase();
+}
+module.exports = upperCaser
+
+function repeat(operation, num) {
+
+}
+
+// Do not remove the line below
+module.exports = repeat
